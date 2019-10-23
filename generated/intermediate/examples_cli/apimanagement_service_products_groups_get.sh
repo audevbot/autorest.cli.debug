079fd5ea39f9983640cad4ce670a57040acdef5a
audevbot <audevbot@microsoft.com>
2019-10-23 06:11:38 +0000

    autogenerated-41

--- generated/intermediate/examples_cli/apimanagement_service_products_groups_get.sh
+++
@@ -1,6 +0,0 @@
-# ApiManagementListProductGroups
-RESOURCE_GROUP="myresourcegroup"
-SERVICE_NAME="myservice"
-PRODUCT_NAME="myproduct"
-
-az resource show --id /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.ApiManagement/service/$SERVICE_NAME/products/$PRODUCT_NAME/groups --api-version 2019-01-01
--- generated/intermediate/examples_cli/apimanagement_service_quotas_get.sh
+++
@@ -1,6 +0,0 @@
-# ApiManagementGetQuotaCounterKeys
-RESOURCE_GROUP="myresourcegroup"
-SERVICE_NAME="myservice"
-QUOTA_NAME="myquota"
-
-az resource show --id /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.ApiManagement/service/$SERVICE_NAME/quotas/$QUOTA_NAME --api-version 2019-01-01
